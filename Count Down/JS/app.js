40b2bf229ed39e1ca10c0e23ea271ef09e7cf137
Vishal <vr15102002@gmail.com>
2023-05-12 22:37:36 +0530

    javascript practices

---
+++ Count Down/JS/app.js
@@ -0,0 +1,26 @@
+const enddate  = "15 august 2023 10:00 PM"
+
+document.getElementById("EndDate").innerText = enddate;
+const inputs = document.querySelectorAll("input")
+
+function clock(){
+    const end = new Date(enddate);
+    const now = new Date();
+    const diff = (end-now)/1000;
+    // 
+    if(diff<0) return;
+    
+    inputs[0].value = Math.floor(diff / 3600 / 24);
+    inputs[1].value = Math.floor((diff/3600)%24);
+    inputs[2].value = Math.floor((diff/60)%60);
+    inputs[3].value = Math.floor(diff) % 60;
+}
+//initial call
+clock()
+
+setInterval(
+    () => {
+        clock()
+    },
+    1000
+)
